d2752b1ba08c48857455758fae074025c0fc736d
ErenJaeger66 <erensnk963@gmail.com>
2022-11-07 20:30:12 -0400

    Parte 4 Proyecto

--- src/components/PagInicio.vue
+++ src/components/PagInicio.vue
@@ -1,12 +1,67 @@
 <template>
-    <div>
-        <h1>Inicio</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <h1>Hola</h1>
+            </div>
+            <div class="col-3">
+                <b-card header="Ingreso al Sistema">
+                    <b-alert
+      variant="danger"
+      dismissible
+      fade
+      :show="error">
+      Datos erroneos!
+    </b-alert>
+                    <b-form-group id="input-group-1" label="Usuario:" label-for="input-1" class="m-3">
+                        <b-form-input id="input-1" type="text" v-model="form.usuario" placeholder="Ingrese el usuario"
+                            required>
+                        </b-form-input>
+                    </b-form-group>
+
+                    <b-form-group id="input-group-2" label="Contraseña:" label-for="input-2" class="m-3">
+                        <b-form-input id="input-2" type="password" v-model="form.contraseña"
+                            placeholder="Ingrese la contraseña" required>
+                        </b-form-input>
+                    </b-form-group>
+                    <b-button class="m-3" href="#" v-on:click="verificar()" variant="primary">Ingresar</b-button>
+                </b-card>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "PagInicio"
+    name: "PagInicio",
+    data() {
+        return {
+            form:{
+                usuario:'',
+                contraseña:''
+            },
+            error:false
+        }
+    },
+    methods: {
+        verificar(){
+            this.axios.post('/usr/login',this.form)
+            .then(r =>{
+                this.axios.defaults.headers.common.authorization = r.data.token;
+                localStorage.setItem('token',r.data.token);
+                this.$store.commit('setIdusr',r.data.userdata.id);
+                this.$store.commit('setUsuario',r.data.userdata.usuario);
+                this.$store.commit('setTipo',r.data.userdata.tipo);
+                this.$store.commit('setNombre',r.data.userdata.nombre);
+                
+                this.$router.push('/usr')               
+            })
+            .catch(e=>{
+                this.error=true
+                console.log(e)
+            })            
+        }        
+    },
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,7 @@ import PagInicio from '@/components/PagInicio.vue'
 import RegistroPost from '@/components/RegistroPost.vue'
 import ListHabilitados from '@/components/ListHabilitados.vue'
 import ListConvocatorias from '@/components/ListConvocatorias.vue'
-
-
+import BienvenidoUsuario from '@/components/BienvenidoUsuario.vue'
 
 Vue.use(VueRouter)
 
@@ -33,16 +32,18 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: 'about',    
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/registrocompleto',
     name: 'registrocompleto',
     component: () => import('../views/RegistroCompleto.vue')
+  },
+  {
+    path: '/usr',
+    name: 'bienvenidoUsr',
+    component: BienvenidoUsuario
   }
 ]
 
